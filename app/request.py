af28466ccd6062c8684260645802aceae381cfe1
Lewis <lewis.murgor@student.moringaschool.com>
2022-02-27 22:47:35 +0300

    add get_article function

--- app/request.py
+++ app/request.py
@@ -1,6 +1,8 @@
 from os import name
 from app import app
 import urllib.request,json
+
+from app.article_test import Article
 from .models import source
 
 Source = source.Source
@@ -9,6 +11,8 @@ Source = source.Source
 api_key = app.config['NEWS_API_KEY']
 
 base_url = app.config['NEWS_API_URL']
+
+article_url = app.config['ARTICLES_API_URL']
 
 def get_news_sources():
     '''
@@ -53,3 +57,26 @@ def process_results(sources_list):
         sources_results.append(sources_object)
 
     return sources_results
+
+def get_article(id):
+
+    get_article_url = article_url.format(id, api_key)
+
+    with urllib.request.urlopen(get_article_url) as url:
+        article_data = url.read()
+        article_response = json.loads(article_data)
+
+        article_object = None
+        if article_response:
+            source = article_response.get('source')
+            author = article_response.get('author')
+            title = article_response.get('title')
+            description = article_response.get('description')
+            url = article_response.get('url')
+            urlToImage = article_response.get('urlToImage')
+            publishedAt = article_response.get('publishedAt')
+            content = article_response.get('content')
+
+            article_object = Article(source, author, title, description, url, urlToImage, publishedAt, content)
+            
+    return article_object
