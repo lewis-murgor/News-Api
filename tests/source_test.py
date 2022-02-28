7667710e96135707afcd9bbe6a3490f41e62c5ee
Lewis <lewis.murgor@student.moringaschool.com>
2022-02-28 23:48:22 +0300

    create tests folder

---
+++ tests/source_test.py
@@ -0,0 +1,20 @@
+import unittest
+from app.models import source
+
+Source = source.Source
+
+class SourceTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the Source class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+
+        self.new_source = Source("abc-news", "ABC News", "Your trusted source for breaking news, analysis, exclusive interviews, headlines and videos at ABCNews.com", "https://abcnews.go.com", "category", "en", "us")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_source,Source))
+
